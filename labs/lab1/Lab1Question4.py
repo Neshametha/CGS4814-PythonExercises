4e370d60bcb21b5e8e3c275ccc59c6ac54052a7e
kpola009 <50279284+kpola009@users.noreply.github.com>
2019-09-16 19:30:36 -0400

    Update Lab1Question4.py

--- labs/lab1/Lab1Question4.py
+++ labs/lab1/Lab1Question4.py
@@ -1,2 +1,8 @@
 ""Ketul Polara""
+name = str(input("What is your Name?"))
+age = int(input("What is your Age?"))
 
+differnce = int(100 - age)
+AfterAge = int(differnce + 2019)
+
+print("Hi " + name + " you will turn 100 years old in year " + str(AfterAge))
